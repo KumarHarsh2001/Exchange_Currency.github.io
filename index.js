0c76ee044690261cb7551824c56b5aeab27636b4
KumarHarsh2001 <kumarharshalok2001@gmail.com>
2022-12-04 01:30:33 +0530

    Mark4 Update in footer.

--- index.js
+++ index.js
@@ -1,52 +1,52 @@
 document.addEventListener('DOMContentLoaded',function(){
-    
-    fetch('https://open.er-api.com/v6/latest/USD')
+        fetch('https://open.er-api.com/v6/latest/USD')
         .then(data=>data.json())
         .then((data)=>{
-            let val_from, val_to,uc_val, acutal_val, uval;
+            let val_from, val_to,uc_val, acutal_val,uval;
             let currency_from,currency_to;
             const CurCode = Object.keys(data.rates);
+            
+            
             CurCode.map(function(key){
                 const option = document.createElement('option');
                 option.value = key;
                 option.innerHTML = key;
-                const ele = document.querySelector('#cur_from').append(option);
+                document.querySelector('#cur_from').append(option);
             })
             CurCode.map(function(key){
                 const option = document.createElement('option');
                 option.innerHTML = key;
-                const ele = document.querySelector('#cur_to').append(option);
+                document.querySelector('#cur_to').append(option);
             })
-            
+            document.querySelector('#btn').disabled = false;
             document.querySelector('#cur_from').onchange = function(){
                 currency_from = this.value;
                 val_from = data.rates["USD"]/data.rates[(this.value)];
             }
+
             document.querySelector('#cur_to').onchange = function(){
                 currency_to = this.value;
                 val_to = data.rates[(this.value)];
-                uc_val = (val_from*val_to).toFixed(2);
+                uc_val = (val_from*val_to);
             }
             document.querySelector('#btn').onclick = function(){
-                    uval = parseInt(document.querySelector('#uinput').value);
-                    acutal_val=(uval * uc_val).toFixed(2);
-                    document.querySelector('#res').innerHTML = `1 ${currency_from} = ${uc_val} ${currency_to}`;
-                    const li = document.createElement('li');
-                    li.innerHTML = `${uval} ${currency_from} is equal to ${acutal_val} ${currency_to}`
-                    document.querySelector('ul').append(li);
-                    document.querySelector('#uinput').value="";
-                    // document.querySelector('#res').value = "";
-                    document.querySelector('#cur_from').value="--select--";
-                    document.querySelector('#cur_to').value = "--select--";
-                    uval="";
-                    currency_from="";
-                    currency_to="";
-                    acutal_val = "";
-            }
-            
+                uval = parseInt(document.querySelector('#uinput').value);
+                acutal_val=(uval * uc_val).toFixed(2);
+                document.querySelector('#res').innerHTML = `1 ${currency_from} = ${uc_val.toFixed(6)} ${currency_to}`;
+                const li = document.createElement('li');
+                li.innerHTML = `${uval} ${currency_from} is equal to ${acutal_val} ${currency_to}`
+                document.querySelector('ul').append(li);
+                document.querySelector('#uinput').value="";
 
-        })
-    
+                document.querySelector('#cur_from').value="--select--";
+                document.querySelector('#cur_to').value = "--select--";
+                uval="";
+                currency_from="";
+                currency_to="";
+                acutal_val = "";
+            } 
+            })
+
     
 })
 
